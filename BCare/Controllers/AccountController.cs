4cf457237dd4e28a574112f358c0bb3aec71dfb7
EladJ <EladJ@DESKTOP-NU6E84U>
2017-05-04 00:53:06 +0300

    Register working.

--- BCare/Controllers/AccountController.cs
+++ BCare/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BCare.data;
 using BCare.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BCare.Controllers
 {
@@ -16,13 +17,22 @@ namespace BCare.Controllers
             context = HttpContext.RequestServices.GetService(typeof(BCare.data.BcareContext)) as BcareContext;
             //context.Register(319253365, "Maria", "Gunko", "F" , "06-01-1992",7, "AB+", "Abraham Ofer 11 Ashdod", "MashaG", "1234", false);
             //List<blood_test> BT = context.GetUserTests(34928267);
-            List<blood_component> BTD = context.GetTestResultByID(1);
+            //List<blood_test_data> BTD = context.GetTestResultByID(1);
             return View();
         }
 
         public IActionResult Register()
         {
+            context = HttpContext.RequestServices.GetService(typeof(BCare.data.BcareContext)) as BcareContext;
+            ViewBag.ListHMO = context.GetAllHMO();
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(int User_ID, string First_Name, string Last_Name, string Gender, string Birth_Date, int HMO_ID, string Blood_Type, string Address, string username, string password, bool isDoctor)
+        {
+            context = HttpContext.RequestServices.GetService(typeof(BCare.data.BcareContext)) as BcareContext;
+            context.Register(User_ID, First_Name, Last_Name, Gender, Birth_Date, HMO_ID, Blood_Type, Address, username, password, isDoctor);
+            return RedirectToAction("Index","Home");
         }
     }
 }
---
+++ BCare/Models/UserDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BCare.Models
+{
+    public class UserDetailViewModel
+    {
+        public User user { get; set; }
+        public premission_type pt { get; set; }
+        public premission_for_users pfu { get; set; }
+        public health_maintenance_organizations hmo { get; set; }
+        public Boolean isDoctor { get; set; }
+    }
+}

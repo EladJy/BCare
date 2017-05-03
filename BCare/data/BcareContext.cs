e19252c4c5448eb1f07cc29485b2f6308bb31cb5
masha.gonko <masha.gonko@gmail.com>
2017-05-03 14:53:07 +0300

    Updated

--- BCare/data/BcareContext.cs
+++ BCare/data/BcareContext.cs
@@ -86,8 +86,9 @@ namespace BCare.data
             using (MySqlConnection conn = GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("INSERT INTO users VALUES (User_ID, First_Name, Last_Name, Gender, Birth_Date, 'FOREIGN KEY (HMO_ID) REFERENCES health_maintenance_organizations (HMO_ID)', Blood_Type, Address) ", conn);
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO users VALUES (User_ID, First_Name, Last_Name, Gender, Birth_Date, HMO_ID, Blood_Type, Address) ", conn);
                 cmd.ExecuteNonQuery();
+                // 'FOREIGN KEY (HMO_ID) REFERENCES health_maintenance_organizations (HMO_ID)'
             }
            // return newUser;
         }
@@ -122,7 +123,7 @@ namespace BCare.data
                             BTestID = reader.GetInt32("BTest_ID"),
                             BUserID = reader.GetInt32("BUser_ID"),
                             BTestDate = reader.GetDateTime("BTest_Date"),
-                            BDocName = reader.GetString("BDoc_Name")
+                            //BDocName = reader.GetString("BDoc_Name")
                         });
                     }
                 }

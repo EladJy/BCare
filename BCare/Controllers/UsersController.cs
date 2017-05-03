c7ffe8f08bf29993606c3042f121c8aa25d2629a
masha.gonko <masha.gonko@gmail.com>
2017-05-03 14:20:18 +0300

    Updated

--- BCare/Controllers/UsersController.cs
+++ BCare/Controllers/UsersController.cs
@@ -10,13 +10,10 @@ namespace BCare.Controllers
     public class UsersController : Controller
     {
         BcareContext context;
-        public UsersController()
-        {
-            context = HttpContext.RequestServices.GetService(typeof(BCare.data.BcareContext)) as BcareContext; 
-        }
         public IActionResult Index()
         {
-            
+            context = HttpContext.RequestServices.GetService(typeof(BCare.data.BcareContext)) as BcareContext;
+            context.SignUp(319253365, "מריה", "גונקו", "Female" , "06-01-1992",7, "AB+", "אברהם עופר 11 אשדוד");
             return View(context.GetAllUsers());
         }
     }
--- BCare/data/BcareContext.cs
+++ BCare/data/BcareContext.cs
@@ -44,6 +44,7 @@ namespace BCare.data
         }
         public bool Login(string username , string password)
         {
+            User user = new User();
             using (MySqlConnection conn = GetConnection())
             {
   
@@ -66,6 +67,92 @@ namespace BCare.data
                 }
             }
             return false;
+        }
+
+        public void SignUp(int User_ID, string First_Name, string Last_Name, string Gender, string Birth_Date, int HMO_ID, string Blood_Type, string Address)
+        {
+            //int userId = GenerateAutoID();
+            // User newUser = new User()
+            //{
+            //    UserID = userId,
+            //    HMOID = HMOID,
+            //    FirstName = firstname,
+            //    LastName = lastname,
+            //    Gender = new Gender (),
+            //    BirthDate = Convert.ToDateTime(birhdate),
+            //    BloodType = new BloodType(),
+            //    Address = address
+            //};
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO users VALUES (User_ID, First_Name, Last_Name, Gender, Birth_Date, 'FOREIGN KEY (HMO_ID) REFERENCES health_maintenance_organizations (HMO_ID)', Blood_Type, Address) ", conn);
+                cmd.ExecuteNonQuery();
+            }
+           // return newUser;
+        }
+
+        private int GenerateAutoID()
+        {
+            int i;
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select Count (User_ID) from users", conn);
+                i = Convert.ToInt32(cmd.ExecuteScalar());
+                conn.Close();
+                i++;
+            }
+            return i;
+        }
+
+        public List<blood_test> GetUserTests(int userId)
+        {
+            List<blood_test> testsForUser = new List<blood_test>();
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from blood_test WHERE BUser_ID=UserID", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        testsForUser.Add(new blood_test()
+                        {
+                            BTestID = reader.GetInt32("BTest_ID"),
+                            BUserID = reader.GetInt32("BUser_ID"),
+                            BTestDate = reader.GetDateTime("BTest_Date"),
+                            BDocName = reader.GetString("BDoc_Name")
+                        });
+                    }
+                }
+            }
+            return testsForUser;
+        }
+
+        public List<blood_test_data> GetTestResulltByID (int testId)
+        {
+            List<blood_test_data> bloodTestResult = new List<blood_test_data>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from blood_test_data WHERE BTest_ID=testID", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        bloodTestResult.Add(new blood_test_data()
+                        {
+                            BTestID = reader.GetInt32("BTest_ID"),
+                            BCompID = reader.GetInt32("BComp_ID"),
+                            Value = reader.GetDouble("Value")
+                        });
+                    }
+                }
+            }
+
+            return bloodTestResult;
         }
     }
 }
